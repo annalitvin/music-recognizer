778a62a91ac743cdcb1f3bb93c2aba3bc96ac240
annalitvin <litvin_any@ukr.net>
2020-01-31 02:58:17 +0200

    Added game

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,13 +1,14 @@
 import requests
 import base64
 
+
 def get_lyrics(lyrics):
 
     if lyrics is not None:
         data = {
             'q': str(lyrics),
             'return':'timecode,apple_music,deezer,spotify',
-            'api_token': 'f2459ae0fc49b06829ff9f11085b9cfa'
+            'api_token': 'f9d05c0e0df19edf712ee220b4e30cac'
             }
     result = requests.post('https://api.audd.io/findLyrics/', data=data)
 
@@ -27,21 +28,22 @@ def get_lyrics(lyrics):
     artist = None
     search_id = None
 
-
+    id_list = []
     for artist_item in artists:
         artist = artist_item.get('track_artist')
         title = artist_item.get('track_title')
         search_id = requests.get(f'https://api.deezer.com/search?q={artist}')
 
         data = search_id.json().get('data')
-        if len(data) > 0:
+        if data is not None and len(data) > 0:
             for data_item in data:
                 title_data = data_item.get('title')
                 artist_data = data_item.get('artist')
                 if artist_data.get('name').lower() == artist.lower():
                     if title_data is not None and title is not None:
                         if title_data.lower() in title.lower() or title.lower() in title_data.lower():
-                            return data_item.get("id")
+                            id_list.append(data_item.get("id"))
+    return id_list
 
 
 def get_audio_file(audio_link):
